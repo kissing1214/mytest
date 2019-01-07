06c69ddd057635c1998ece215bc793e8bd87ef5a
kissi <kissi@LAPTOP-EONLV6GG>
2019-01-07 17:13:52 +0900

    person class 변경

--- mytest/src/com/herbmall/test/Person.java
+++ mytest/src/com/herbmall/test/Person.java
@@ -26,6 +26,6 @@ public class Person {
 	}
 	
 	public void showInfo() {
-		System.out.println("Person클래스의 showInfo()메서드!!");
+		System.out.println("Person클래스의 showInfo()메서드!!");//내용바뀜
 	}
 }//class
